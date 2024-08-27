40744f21cd6dbe54f8f0591e4e1fe7c2244595d4
Fabio Cortés <fcor14@hotmail.com>
2024-08-27 16:10:32 -0700

    Base drawing

--- src/script.js
+++ src/script.js
@@ -2,12 +2,18 @@ import * as THREE from "three";
 import { TubePainter } from "three/examples/jsm/misc/TubePainter.js";
 import { XRButton } from "three/examples/jsm/webxr/XRButton.js";
 import { XRControllerModelFactory } from "three/examples/jsm/webxr/XRControllerModelFactory.js";
+import { XRHandModelFactory } from "three/examples/jsm/webxr/XRHandModelFactory.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 
 let camera, scene, renderer;
 let controller1, controller2;
 let controllerGrip1, controllerGrip2;
+let hand1, hand2;
+let painter1, painter2;
+let handsOn = false;
+
+const cursor = new THREE.Vector3();
 
 const sizes = {
   width: window.innerWidth,
@@ -42,10 +48,16 @@ function init() {
     side: THREE.DoubleSide,
   });
 
-  const geometry = new THREE.CylinderGeometry(0.005, 0.005, 0.05);
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(0, 1.5, -0.5);
-  scene.add(mesh);
+  painter1 = new TubePainter();
+  painter1.mesh.material = material;
+  painter1.setSize(0.3);
+
+  painter2 = new TubePainter();
+  painter2.mesh.material = material;
+  painter2.setSize(0.3);
+
+  scene.add(painter1.mesh);
+  scene.add(painter2.mesh);
 
   renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
   renderer.setPixelRatio(window.devicePixelRatio, 2);
@@ -53,41 +65,47 @@ function init() {
   renderer.setAnimationLoop(animate);
   renderer.xr.enabled = true;
   document.body.appendChild(
-    XRButton.createButton(
-      renderer
-      //   {
-      //   optionalFeatures: ["depth-sensing"],
-      //   depthSensing: { usagePreference: ["gpu-optimized"], dataFormatPreference: [] },
-      // }
-    )
+    XRButton.createButton(renderer, {
+      optionalFeatures: ["hand-tracking", "depth-sensing"],
+      depthSensing: { usagePreference: ["gpu-optimized"], dataFormatPreference: [] },
+    })
   );
 
   const controllerModelFactory = new XRControllerModelFactory();
-
-
+  // Controllers
   controller1 = renderer.xr.getController(0);
   controller1.addEventListener("connected", onControllerConnected);
-  controller1.addEventListener("selectstart", onSelectStart);
-  controller1.addEventListener("selectend", onSelectEnd);
-  controller1.addEventListener("squeezestart", onSqueezeStart);
-  controller1.addEventListener("squeezeend", onSqueezeEnd);
+  scene.add(controller1);
+
+  controller2 = renderer.xr.getController(1);
+  controller2.addEventListener("connected", onControllerConnected);
+  scene.add(controller2);
+
+  const handModelFactory = new XRHandModelFactory();
+
+  // Hand1
   controllerGrip1 = renderer.xr.getControllerGrip(0);
   controllerGrip1.add(controllerModelFactory.createControllerModel(controllerGrip1));
   scene.add(controllerGrip1);
-  scene.add(controller1);
 
+  hand1 = renderer.xr.getHand(0);
+  // hand1.add(handModelFactory.createHandModel(hand1, "mesh"));
+  hand1.addEventListener("pinchstart", onPinchStart);
+  hand1.addEventListener("pinchend", onPinchEnd);
+  hand1.userData.painter = painter1;
+  scene.add(hand1);
 
-  controller2 = renderer.xr.getController(1);
-  controller1.addEventListener("connected", onControllerConnected);
-  controller2.addEventListener("selectstart", onSelectStart);
-  controller2.addEventListener("selectend", onSelectEnd);
-  controller2.addEventListener("squeezestart", onSqueezeStart);
-  controller2.addEventListener("squeezeend", onSqueezeEnd);
+  // Hand2
   controllerGrip2 = renderer.xr.getControllerGrip(1);
   controllerGrip2.add(controllerModelFactory.createControllerModel(controllerGrip2));
   scene.add(controllerGrip2);
-  scene.add(controller2);
 
+  hand2 = renderer.xr.getHand(1);
+  // hand2.add(handModelFactory.createHandModel(hand2, "mesh"));
+  hand2.addEventListener("pinchstart", onPinchStart);
+  hand2.addEventListener("pinchend", onPinchEnd);
+  hand2.userData.painter = painter2;
+  scene.add(hand2);
 }
 
 window.addEventListener("resize", () => {
@@ -105,25 +123,44 @@ window.addEventListener("resize", () => {
 });
 
 function animate() {
+  handleHand(hand1);
+  handleHand(hand2);
+
   // Render
   renderer.render(scene, camera);
 }
 
-function onSqueezeStart(e) {
-  console.log(e);
+function handleHand(hand) {
+  if (handsOn === false) return;
+
+  hand.updateMatrixWorld(true);
+
+  const userData = hand.userData;
+  const painter = userData.painter;
+
+  const indexTip = hand.joints["index-finger-tip"];
+
+  cursor.setFromMatrixPosition(indexTip.matrixWorld);
+
+  if (userData.isDrawing === true) {
+    painter.lineTo(cursor);
+    painter.update();
+  }
 }
-function onSqueezeEnd(e) {
-  console.log(e);
+
+function onPinchStart(e) {
+  this.updateMatrixWorld(true);
+
+  const controller = e.target;
+  const indexTip = controller.joints["index-finger-tip"];
+  cursor.setFromMatrixPosition(indexTip.matrixWorld);
+  this.userData.painter.moveTo(cursor);
+  this.userData.isDrawing = true;
+}
+function onPinchEnd(e) {
+  this.userData.isDrawing = false;
 }
 
 function onControllerConnected(e) {
-  console.log(e);
-}
-
-function onSelectStart(e) {
-  console.log(e);
-}
-
-function onSelectEnd(e) {
-  console.log(e);
+  handsOn = true;
 }
